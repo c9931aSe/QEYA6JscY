1d9903cb4f0de43dc1bce31cedb40efa7ff2867a
c9931aSe <joust.runway-0h@icloud.com>
2025-07-31 22:22:21 +0900

    Add 文档格式转换器 using SPRINGBOOT

---
+++ DocumentConverterApplication_0731_2122_zka.java
@@ -0,0 +1,12 @@
+// 代码生成时间: 2025-07-31 21:22:20
+package com.example.documentconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentConverterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentConverterApplication.class, args);
+    }
+}
