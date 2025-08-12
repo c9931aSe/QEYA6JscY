45e0020b05b54c55f0d3646f35e1fa9f9bc24075
c9931aSe <joust.runway-0h@icloud.com>
2025-08-13 04:54:43 +0900

    Enhance new 测试报告生成器 feature

---
+++ TestReportGeneratorApplication_0813_0354_xie.java
@@ -0,0 +1,13 @@
+// 代码生成时间: 2025-08-13 03:54:42
+package com.example.testreportgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestReportGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestReportGeneratorApplication.class, args);
+    }
+}
