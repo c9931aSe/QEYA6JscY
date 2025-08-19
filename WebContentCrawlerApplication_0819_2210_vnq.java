09831feaa1e6662b9a6c54c3e0738cd334883460
c9931aSe <joust.runway-0h@icloud.com>
2025-08-19 23:10:01 +0900

    Build 网页内容抓取工具 using SPRINGBOOT

---
+++ WebContentCrawlerApplication_0819_2210_vnq.java
@@ -0,0 +1,14 @@
+// 代码生成时间: 2025-08-19 22:10:00
+package com.example.webcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebContentCrawlerApplication {
+# NOTE: 重要实现细节
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebContentCrawlerApplication.class, args);
+    }
+}
