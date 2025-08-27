f97a2c9f85b55807e8b6520cebbc54a0a8d3a772
c9931aSe <joust.runway-0h@icloud.com>
2025-08-27 23:09:48 +0900

    Build 文件备份和同步工具 using SPRINGBOOT

---
+++ FileBackupAndSyncApplication_0827_2209_zsk.java
@@ -0,0 +1,11 @@
+// 代码生成时间: 2025-08-27 22:09:47
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileBackupAndSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileBackupAndSyncApplication.class, args);
+    }
+}
