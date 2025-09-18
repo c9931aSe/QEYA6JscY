c2a059e16ecb914966be03b92e446cca83383e11
c9931aSe <joust.runway-0h@icloud.com>
2025-09-18 15:05:38 +0900

    Add JAVA 文件备份和同步工具

---
+++ FileBackupAndSyncApplication_0918_1405_uhx.java
@@ -0,0 +1,11 @@
+// 代码生成时间: 2025-09-18 14:05:37
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileBackupAndSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileBackupAndSyncApplication.class, args);
+    }
+}
